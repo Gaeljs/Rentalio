0017fa9a17fd16cbf3a8497e8147539f73f183c5
Gaelelm <elm.gaelpro@gmail.com>
2023-05-13 17:40:57 +0200

    Alpha v3

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\RegistrationController;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
+    #[Route('', name: 'app_main')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
